72f3ce4200a8fbe0f4427b6f7ef73322f4930ec9
spaceowlsoul <yuliasunny007@gmail.com>
2022-10-22 10:32:32 +0300

    stage 3/7 complete

--- flashcards/flashcards.py
+++ flashcards/flashcards.py
@@ -1,3 +1,13 @@
-term, definition, answer = (input() for _ in range(3))
-result = 'right!' if answer == definition else 'wrong...'
-print(f'Your answer is {result}')
+cards = int(input('Input the number of cards:\n'))
+dictionary = {}
+
+for card in range(1, cards + 1):
+    term = input(f'The term for card #{card}:\n')
+    definition = input(f'The definition for card #{card}:\n')
+    dictionary[term] = definition
+for key in dictionary:
+    answer = input(f'Print the definition of "{key}":\n')
+    if answer == dictionary[key]:
+        print('Correct!')
+    else:
+        print(f'Wrong. The right answer is "{dictionary[key]}".')
